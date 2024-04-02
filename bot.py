682a513b44137a8b2aa32f515ce3217ae88252e4
SYYANI <40732861+SYYANI@users.noreply.github.com>
2024-04-02 15:41:51 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import json
 import requests
 import logging
-
 from telegram import  Update
 from telegram.ext import (
     Updater,
@@ -18,6 +17,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+user_modes = 'word'
 
 def start(update: Update, context: CallbackContext) -> int:
     """
@@ -25,39 +25,64 @@ def start(update: Update, context: CallbackContext) -> int:
     """
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="这里是一个词典(˵¯͒¯͒˵)"
+        text="这里是一个词典(˵¯͒ ¯͒˵)"
     )
 
 def echo(update: Update, context: CallbackContext) -> None:
     """
     This function would be added to the dispatcher as a handler for messages coming from the Bot API
     """
-    # Print to console
-    # print(f'{update.message.from_user.first_name} wrote {update.message.text}')
-    text = update.message.text
-    url = "http://dict.youdao.com/suggest?q="+text+"&le=eng&num=5&ver=&doctype=json&keyfrom=&model=&mid=&imei=&vendor=&screen=&ssid=&abtest="
-    response = requests.request("GET", url)
-    data = json.loads(response.text)
+    global user_modes
+    if(user_modes == 'word'):
+        text = update.message.text
+        url = "http://dict.youdao.com/suggest?q="+text+"&le=eng&num=5&ver=&doctype=json&keyfrom=&model=&mid=&imei=&vendor=&screen=&ssid=&abtest="
+        response = requests.request("GET", url)
+        data = json.loads(response.text)
+        
+        if data["result"]["code"] == 200:
+            entries = data["data"]["entries"]
+            result_list = []
+            result_list.append(f"查询：'{data['data']['query']}'\n\n")
+            result_list.append("词典释义:\n")
+            for i, entry in enumerate(entries, 1):
+                result_list.append(f"{i}. {entry['entry']}: {entry['explain']}\n")
+            result_str = "".join(result_list)
+        else:
+            result_str = "只能查单词哦(。•́︿•̀。)"
 
-    # 检查返回的结果是否成功
-    if data["result"]["code"] == 200:
-        entries = data["data"]["entries"]
-        result_list = []
-        result_list.append(f"查询：'{data['data']['query']}'\n\n")
-        result_list.append("词典释义:\n")
-        for i, entry in enumerate(entries, 1):
-            result_list.append(f"{i}. {entry['entry']}: {entry['explain']}\n")
-        result_str = "".join(result_list)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=result_str)
     else:
-        result_str = "只能查单词哦(。•́︿•̀。)"
+        url = "https://api.deeplx.org/translate"
+        payload = {
+            "text": update.message.text,
+            "source_lang": 'en',
+            "target_lang": 'zh'
+        }
+        response = requests.post(url, json=payload)
+        result = response.json()
+        if result['code'] != 200:
+            result_str = "翻译失败(。•́︿•̀。)"
+        else:
+            result_str = result['data']
+        context.bot.send_message(chat_id=update.effective_chat.id, text=result_str)
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text=result_str)
+def switch(update: Update, context: CallbackContext) -> None:
+    """
+    This function would be added to the dispatcher as a handler for messages coming from the Bot API
+    """
+    global user_modes
+    if(user_modes == 'word'):
+        user_modes = 'sentence'
+        context.bot.send_message(chat_id=update.effective_chat.id, text="已切换到句子模式")
+    else:
+        user_modes = 'word'
+        context.bot.send_message(chat_id=update.effective_chat.id, text="已切换到单词模式")
 
 def main(token) -> None:
     """Run the bot."""
     # Create the Updater and pass it your bot's token.
     updater = Updater(token, request_kwargs={
-        'proxy_url': 'socks5://127.0.0.1:10808/'
+        'proxy_url': 'socks5://127.0.0.1:1080/'
     })
     # Set your proxy address here
 
@@ -67,6 +92,10 @@ def main(token) -> None:
     # Add start handler
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
+
+    # Add switch handler
+    switch_handler = CommandHandler('switch', switch)
+    dispatcher.add_handler(switch_handler)
 
     dispatcher.add_handler(MessageHandler(~Filters.command, echo))
     # Start the Bot
